d312381cdb13d0046c9d35052281b7a9befebd8b
DWX <nododo1027@gmail.com>
2015-11-02 17:00:18 +0800

    Initial Commit

---
+++ 3D-Touch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  3D-Touch
+//
+//  Created by 杜维欣 on 15/11/2.
+//  Copyright © 2015年 杜维欣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 3D-Touch/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  3D-Touch
+//
+//  Created by 杜维欣 on 15/11/2.
+//  Copyright © 2015年 杜维欣. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
